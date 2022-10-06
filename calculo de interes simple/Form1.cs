0f1be746db4d68ffc9059c29cbbc5ba532ec13d2
LUIS ANGEL ESCOBAR CONSTANZA <SMSS144922@ugb.edu.sv>
2022-10-05 22:22:14 -0300

    calculo de  interes simple

--- calculo de interes simple/Form1.cs
+++ calculo de interes simple/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace calculo_de_interes_simple
+namespace Cálculo_del_interés_simple
 {
     public partial class Form1 : Form
     {
@@ -16,5 +16,44 @@ namespace calculo_de_interes_simple
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+
+            double num1 = double.Parse(txtNum1.Text);
+            double num2 = double.Parse(txtNum2.Text);
+            double num3 = double.Parse(txtNum3.Text);
+
+            string itemSelectd = comboPeriodo.Text;
+            switch (itemSelectd)
+            {
+                case "Anual":
+                    var anual = num1 * num2 * num3;
+                    MessageBox.Show(anual.ToString());
+
+                    break;
+
+                case "Mensual":
+                    var mensual = num1 * num2 * num3 / 12;
+                    MessageBox.Show(mensual.ToString());
+                    break;
+
+                case "Diario":
+                    var diario = num1 * num2 * num3 / 360;
+                    MessageBox.Show(diario.ToString());
+                    break;
+
+ 
+            }
+
+        }
+
+        }
     }
-}
+
